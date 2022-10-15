b39a88ef87df10088d95f8df5beb8d3bf0afb7c0
mordjane <78384914+mordjanee@users.noreply.github.com>
2022-10-16 01:13:46 +0200

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
 function generateRandomColor() {
     var color = "#" + Math.random().toString(16).slice(-3);
-    document.body.style.backgroundColor.setProperty("background-color", color);
+    document.body.style.background = color;
   }
