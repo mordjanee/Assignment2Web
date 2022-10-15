cd8c23538ac2e3b15359b7d4b493bb95fd530ec6
mordjane <78384914+mordjanee@users.noreply.github.com>
2022-10-16 01:42:31 +0200

    Add files via upload

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,21 @@ function generateRandomColor() {
     var color = "#" + Math.random().toString(16).slice(-3);
     document.body.style.background = color;
   }
+
+const dog_btn = document.getElementById('dog_btn');
+const dog_result = document.getElementById('dog_result');
+
+dog_btn.addEventListener('click', getRandomDog);
+
+function getRandomDog() {
+    fetch('https://random.dog/woof.json')
+        .then(res => res.json())
+        .then(data => {
+            if(data.url.includes('.mp4')) {
+                getRandomDog();
+            }
+            else {
+                dog_result.innerHTML = `<img src=${data.url} alt="dog" />`;
+            }
+        });
+}
